6b15d8ce97ff2f20cc5eaeb74d234c6a86c46876
SizheQiu <siq001@DESKTOP-OL261DO.localdomain>
2021-11-21 20:11:19 +0000

    add_DMEME_medium

--- workflow/ccfba_functions.py
+++ workflow/ccfba_functions.py
@@ -6,38 +6,35 @@ import numpy as np
 
 
 cofactors = ['nad','atp','gtp','ctp','utp','ttp',
-             'fad','adp','gdp','cdp','udp','tdp','coa']
+             'fad','adp','gdp','cdp','udp','tdp']
 macro = ['quino','cytoch','heme']
+non_met = [ 'EX_','sink_', 'DM_', 'Growth','biomass'  ]
 
-
-def checkMet( met ):
-    """
-    check whether metabolite contributes to carbon flux
-    """
+def isCofactor( met ):
     for cof in cofactors:
         if cof in met.id:
-            return False
+            return True
     for ma in macro:
         if ma in met.name:
-            return False
-    return True
+            return True
+    return False
 
-def addBound(model, rxn_id, num_atom, f_total ):
-    """
-    apply ub and lb for the reaction.
-    """
-    if num_atom == 0:
-        return 0
-    else:
-        temp_rxn = model.reactions.query( rxn_id )
-        if temp_rxn.reversibility == True:
-            temp_rxn.lower_bound = - (f_total/num_atom)
-            temp_rxn.upper_bound = f_total/num_atom
-        else:
-            temp_rxn.upper_bound = f_total/num_atom
-            
-    return 1
+def isCoA( met ):
+    if 'coa' in met.id:
+        return True
+    return False
+    
+def isACP( met ):
+    if 'ACP' in met.id:
+        return True
+    return False
 
+def isNonMet( rxn ):
+    for prefix in non_met:
+        if prefix in rxn.id:
+            return True
+    return False
+    
 
 def getCNCount( rxn ):
     """
@@ -45,13 +42,21 @@ def getCNCount( rxn ):
     """
     C,N = 0,0
     for met in rxn.reactants:
-        if checkMet( met ):
+        if ! (isCofactor( met ) ):
             coeff =  abs(rxn.get_coefficient(met.id))
-            C += int(coeff)*met.elements['C']
-            N += int(coeff)*met.elements['N']
+            if isCoA( met ):
+                C += int(coeff)*( met.elements['C'] - 21 )
+                N += int(coeff)*( met.elements['N'] - 7 )
+            elif isACP( met ):
+                C += int(coeff)*( met.elements['C'] - 11 )
+                N += int(coeff)*( met.elements['N'] - 2 )
+            else:
+                C += int(coeff)*met.elements['C']
+                N += int(coeff)*met.elements['N']
+                
     return {'C':C,'N':N}
             
-    
+
 def getCN_influx(model):
     C_in,N_in = 0,0
     for exchange_id in model.medium:
@@ -61,14 +66,32 @@ def getCN_influx(model):
         N_in += CN_count['N']*model.medium[exchange_id]
     return {'C':C_in,'N':N_in}
         
+
+    
+def addBound(model, rxn_id, num_atom, f_total ):
+    """
+    apply ub and lb for the reaction.
+    """
+    if num_atom == 0:
+        return 0
+
+    temp_rxn = model.reactions.query( rxn_id )
+    bound = f_total/num_atom
+    old_lb, old_ub = temp_rxn.lower_bound, temp_rxn.upper_bound
+    if temp_rxn.reversibility == True:
+        temp_rxn.lower_bound = max( - bound, old_lb )
+        temp_rxn.upper_bound = min( bound, old_ub )
+    else:
+        temp_rxn.upper_bound = min( bound, old_ub )
+            
+    return 1
               
 
 def ccfba(model):
     
     C_in, N_in = getCN_influx(model)['C'], getCN_influx(model)['N']
-    
     for rxn in model.reactions:
-        if 'EX_' in rxn.id or 'sink_' in rxn.id or 'Growth' in rxn.id:
+        if  isNonMet(rxn):
             continue
         else:
             CN_count = getCNCount(rxn)
