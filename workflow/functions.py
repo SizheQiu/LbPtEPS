f27261c0297bdb8b54ace98676e919729a22ddf5
SizheQiu <sizheqiu001@gmail.com>
2023-12-11 22:52:32 +0000

    update_11122023

--- workflow/functions.py
+++ workflow/functions.py
@@ -24,6 +24,27 @@ def volcano( table, lfc_col, pv_col, lfc_cutoff, pv_cutoff, ax):
     ax.axvline(x=x_cut2, color='grey', linestyle='--')
     return ax
 
+def annot_topn( table, n_top, name_dict, lfc_col, pv_col, lfc_cutoff, pv_cutoff, fontsize, ax ):
+    x_cut1 = lfc_cutoff; x_cut2 = - lfc_cutoff;
+    y_cut = -np.log10( pv_cutoff )
+    sig_table = table[ ( (table[lfc_col]>x_cut1) | (table[lfc_col]<x_cut2) ) & (table[pv_col]<pv_cutoff)  ]
+    sig_table = sig_table.reset_index().drop(['index'],axis=1)
+    up_table = sig_table[sig_table[lfc_col]>0].sort_values(by=[lfc_col],ascending=False).\
+                                    reset_index().drop(['index'],axis=1)
+    down_table = sig_table[sig_table[lfc_col]<0].sort_values(by=[lfc_col],ascending=True).\
+                                      reset_index().drop(['index'],axis=1)
+    
+    for i in range( min(n_top,len(up_table.index)) ):
+        ax.text(1.01* up_table[lfc_col][i], -np.log10(up_table[pv_col][i]),\
+                s= name_dict[up_table['ID'][i]], fontsize=fontsize)
+        
+    for i in range( min(n_top,len(down_table.index)) ):
+        ax.text(1.01* down_table[lfc_col][i], -np.log10(down_table[pv_col][i]),\
+                s= name_dict[down_table['ID'][i]], fontsize=fontsize)   
+        
+    return ax
+
+
 def annot_volcano( table, name_dict, lfc_col, pv_col, lfc_cutoff, pv_cutoff, fontsize, ax ):
     x_cut1 = lfc_cutoff; x_cut2 = - lfc_cutoff;
     y_cut = -np.log10( pv_cutoff )
@@ -38,6 +59,12 @@ def annot_volcano( table, name_dict, lfc_col, pv_col, lfc_cutoff, pv_cutoff, fon
         ax.text(1.01*x_list[i], y_list[i], s=names[i], fontsize=fontsize)
     return ax
 
+def get_deg(table, lfc_col, pv_col, lfc_cutoff, pv_cutoff):
+    x_cut1 = lfc_cutoff; x_cut2 = - lfc_cutoff;
+    y_cut = -np.log10( pv_cutoff )
+    sig_table = table[ ( (table[lfc_col]>x_cut1) | (table[lfc_col]<x_cut2) ) & (table[pv_col]<pv_cutoff)  ]
+    sig_table = sig_table.reset_index().drop(['index'],axis=1)
+    return {'up':list(sig_table[sig_table[lfc_col]>0]['ID']),'down':list(sig_table[sig_table[lfc_col]<0]['ID'])}
 
 
 
